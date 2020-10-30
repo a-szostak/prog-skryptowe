98c9542b08787f160abbdfe1cf31e902aefbcf49
a-szostak <agnieszka.szostak00@gmail.com>
2020-10-30 13:25:49 -0700

    1, partly 2

--- lab3/DeanerySystem/term.py
+++ lab3/DeanerySystem/term.py
@@ -3,7 +3,7 @@ import re
 from day import Day
 
 class Term:
-    
+
     def __init__(self, day, hour, minute, duration = 90):
         self.duration  = duration
         self.minute = minute
@@ -63,10 +63,17 @@ class Term:
         return False
 
 
-term1 = Term(Day.TUE, 9, 45)
+class Diff(Term):
+    def __init__(self, days, hours, minutes, seconds):
+        pass
+
+
+
+
+'''term1 = Term(Day.TUE, 9, 45)
 print(term1)                     # Ma się wypisać: "Wtorek 9:45 [90]"
 term2 = Term(Day.WED, 10, 15)
 print(term2)                     # Ma się wypisać: "Środa 10:15 [90]"
 print(term1.earlierThan(term2)); # Ma się wypisać: "True"
 print(term1.laterThan(term2));   # Ma się wypisać: "False"
-print(term1.equals(term2));      # Ma się wypisać: "False"
+print(term1.equals(term2));      # Ma się wypisać: "False"'''
---
+++ lab4/DeanerySystem/lesson.py
@@ -0,0 +1,96 @@
+
+from term import Term
+from day import Day
+
+def check_full_time(term: Term):
+
+    if term.day.value in [1,2,3,4] and (term.hour >= 8 and term.hour < 20):
+        return True
+    elif term.day.value == 5 and (term.hour >= 8 and term.hour < 17):
+        return True
+    elif term.day.value in [6,7] and (term.hour >= 8 and term.hour < 20):
+        return False
+    elif term.day.value == 5 and (term.hour >= 17 and term.hour < 20):
+        return False
+
+
+
+class Lesson():
+    def __init__(self, term: Term, name: str, teacherName: str, year: int):
+        self.term = term
+        self.name = name
+        self.teacherName = teacherName
+        self.year = year
+        self.full_time = check_full_time(term)
+
+    def earlierDay(self):
+        if check_full_time(self.term) == True and self.term.day.value != 1:
+            new_day = self.term.day.value - 1
+            new_term = Term(self.term.hour, self.term.minute, day = Day(new_day))
+            print(new_term.day)
+
+        elif check_full_time(self.term) == False and self.term.day.value != 5:
+            new_day = self.term.day.value - 1
+            new_term = Term(self.term.hour, self.term.minute, day = Day(new_day))
+            print(new_term.day)
+
+        else:
+            return False
+
+        return new_term
+
+    def laterDay(self):
+        if check_full_time(self.term) == True and self.term.day.value != 5:
+            new_day = self.term.day.value + 1
+            new_term = Term(self.term.hour, self.term.minute, day = Day(new_day))
+
+
+        elif check_full_time(self.term) == False and self.term.day.value != 7:
+            new_day = self.term.day.value + 1
+            new_term = Term(self.term.hour, self.term.minute, day = Day(new_day))
+
+        else:
+            return False
+
+        return new_term
+
+
+    def earlierTime(self):
+        min = self.term.hour * 60 + self.term.minute
+        new_hour = (min - self.term.duration) // 60
+        new_min = (min - self.term.duration) % 60
+
+        new_term = Term(new_hour, new_min, day = self.term.day)
+
+        if check_full_time(new_term) != None:
+            return new_term
+        else:
+            return False
+
+    def laterTime(self):
+        min = self.term.hour * 60 + self.term.minute
+        new_hour = (self.term.duration + min) // 60
+        new_min = (self.term.duration + min) % 60
+
+        new_term = Term(new_hour, new_min, day = self.term.day)
+
+        if check_full_time(new_term) != None:
+            return new_term
+        else:
+            return False
+
+
+    def __str__(self):
+        week = {"1":"Poniedzialek", "2":"Wtorek","3":"Sroda","4":"Czwartek","5":"Piatek","6":"Sobota","7":"Niedziela"}
+
+        godz = self.term.duration // 60
+        min = self.term.duration % 60
+        end_hour = self.term.hour + godz + (min + self.term.minute) // 60
+        end_min = (min + self.term.minute) % 60
+
+        adjectives = {"1":"Pierwszy", "2":"Drugi", "3":"Trzeci", "4":"Czwarty", "5":"Piaty"}
+        studies = {"True":"stacjonarnych", "False":"niestacjonarnych"}
+
+        return self.name + " (" + week[str(self.term.day.value)] + " " + str(self.term.hour) \
+        + ":" + str(self.term.minute) + " - " + str(end_hour) + ":" + str(end_min) + ")" + "\n" \
+        + adjectives[str(self.year)] + " rok studiow " +  studies[str(check_full_time(self.term))]
