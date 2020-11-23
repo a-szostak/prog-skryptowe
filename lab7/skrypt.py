b5465850ab83427899879d1c2fba3ab3fb7994f1
a-szostak <agnieszka.szostak00@gmail.com>
2020-11-23 04:32:19 -0800

    lab7

--- lab7/skrypt.py
+++ lab7/skrypt.py
@@ -2,22 +2,18 @@ import random
 import itertools
 import re
 
-kod1 = random.randint(33,58)
-kod2 = random.randint(33,58)
-dlugosc = 3#random.randint(1,2)
+kod1 = random.randint(0,127)
+kod2 = random.randint(0,127)
+dlugosc = random.randint(1,5)
 
-print(kod1,kod2,dlugosc)
 
 x = list(map(lambda a: chr(a),range(min(kod1,kod2),max(kod1,kod2)+1)))
 
-
 comb = list(map(lambda b: ''.join(b), list(itertools.combinations_with_replacement(x,dlugosc))))
 
-
-
-q = list(filter(lambda c: re.match('^[0-9]*', c).group(),comb))
-print(q)
+numbers = list(filter(lambda c: re.match('^[0-9]*', c).group(),comb))
 
 tab = []
-w = list(filter(lambda d: tab.append(d) and len(re.match('^[0-9]*', d).group()) <= dlugosc-1, q))
+list(filter(lambda d: tab.append(d) and len(re.match('[0-9]*', d)) <= dlugosc-1, numbers))
+
 print(tab)
