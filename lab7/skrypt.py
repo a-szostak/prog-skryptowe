4c550d855e65ca995cb86934cc2bb19ef2e5e795
a-szostak <agnieszka.szostak00@gmail.com>
2020-11-23 09:08:26 -0800

    ch-ch-changes

--- lab7/skrypt.py
+++ lab7/skrypt.py
@@ -2,8 +2,8 @@ import random
 import itertools
 import re
 
-kod1 = random.randint(0,127)
-kod2 = random.randint(0,127)
+kod1 = random.randint(0,128)
+kod2 = random.randint(0,128)
 dlugosc = random.randint(1,5)
 
 
@@ -14,6 +14,7 @@ comb = list(map(lambda b: ''.join(b), list(itertools.combinations_with_replaceme
 numbers = list(filter(lambda c: re.match('^[0-9]*', c).group(),comb))
 
 tab = []
-list(filter(lambda d: tab.append(d) and len(re.match('[0-9]*', d)) <= dlugosc-1, numbers))
+list(filter(lambda d: len(re.match('[0-9]*', d).group()) <= (dlugosc-1) and tab.append(d), numbers))
 
 print(tab)
+
