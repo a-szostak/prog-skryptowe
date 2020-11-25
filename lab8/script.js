6069a3590a083ac4297c693459c83f18c05a2a27
a-szostak <agnieszka.szostak00@gmail.com>
2020-11-25 21:40:18 +0100

    counting functions

--- lab8/script.js
+++ lab8/script.js
@@ -1,21 +1,54 @@
-window.prompt("Tekst1","Tekst2");
+"use strict";
+
+var expect = chai.expect;
+
+function sum(x,y) {
+	return x+y;
+}
+
+licz();
+
+function cyfry(napis){
+	napis = napis.split('');
+	var suma = 0;
+	for (var i=0; i < napis.length; i++){
+		if (Number.isInteger(Number(napis[i]))){
+			suma = suma + Number(napis[i]);
+		}
+	}
+	return suma;
+}
 
 
-var body = document.querySelector('body');
-body.onload = function(){
-          console.log('Tekst 1');
-          window.alert('Tekst 2');
-        }
+function litery(napis){
+	napis = napis.split('');
+	var suma = 0;
+	for (var i=0; i < napis.length; i++){
+		if (!Number.isInteger(Number(napis[i]))){
+			suma++;
+		}
+	}
+	return suma;
+}
 
 
+function licz(){
+	var suma = 0;
+	while(true){
+		var napis = window.prompt("Wpisz coś");
+		if (napis === null){
+			break;
+		}
+		suma += cyfry(napis);
+		console.log(cyfry(napis) + ' ' + litery(napis) + ' ' + suma);
+	}
+}
 
-var button = document.getElementById('wypisz');
-
-button.onclick = function() {
-    var txt = document.forms[0].elements['pole_tekstowe'].value;
-    var num =  document.forms[0].elements['pole_liczbowe'].value;
-
-    document.getElementById('formElements').innerText = txt + "\nTyp: " + typeof(txt);
-    document.getElementById('formElements').innerText += "\n" + num + "\nTyp: " + typeof(txt);
-
-  }
+describe('The sum() function', function() {
+ it('Returns 4 for 2+2', function() {
+   expect(sum(2,2)).to.equal(4);
+ });
+ it('Returns 0 for -2+2', function() {
+   expect(sum(-2,2)).to.equal(0);
+ });
+});
---
+++ lab8/script_zad1.js
@@ -0,0 +1,21 @@
+window.prompt("Tekst1","Tekst2");
+
+
+var body = document.querySelector('body');
+body.onload = function(){
+          console.log('Tekst 1');
+          window.alert('Tekst 2');
+        }
+
+
+
+var button = document.getElementById('wypisz');
+
+button.onclick = function() {
+    var txt = document.forms[0].elements['pole_tekstowe'].value;
+    var num =  document.forms[0].elements['pole_liczbowe'].value;
+
+    document.getElementById('formElements').innerText = txt + "\nTyp: " + typeof(txt);
+    document.getElementById('formElements').innerText += "\n" + num + "\nTyp: " + typeof(txt);
+
+  }
